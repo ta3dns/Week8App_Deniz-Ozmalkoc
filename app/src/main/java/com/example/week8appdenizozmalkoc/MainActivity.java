970e1df6613e1e52316e7ccb7d51eeb6b48e0327
ta3dns <deniz@ozmalkoc.com>
2025-03-09 16:52:59 +0200

    Added comments to better explain the code.

--- app/src/main/java/com/example/week8appdenizozmalkoc/MainActivity.java
+++ app/src/main/java/com/example/week8appdenizozmalkoc/MainActivity.java
@@ -13,6 +13,16 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/* **********************************************************************************************
+ Deniz Özmalkoc
+ OOP Week 8 Task
+ 09.03.2025
+ https://github.com/ta3dns/Week8App_Deniz-Ozmalkoc
+
+ Android app using java that performs basic arithmetic operations on two numbers.
+ ************************************************************************************************/
+
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText inputFirstNumber;
@@ -20,9 +30,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView output;
     private float firstNumberInt;
     private float secondNumberInt;
-
-
     private float result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,84 +48,104 @@ public class MainActivity extends AppCompatActivity {
         inputFirstNumber = findViewById(R.id.inputFirstNumber);
         inputSecondNumber = findViewById(R.id.inputSecondNumber);
         output = findViewById(R.id.outputView);
+        // Initialize the input and output fields.
 
     }
 
-
-
-
     public void sumFunction(View view){
-        try {
+        // Sum Function: Gets Called when the + button is pressed. Adds the two numbers together and displays the result.
+
+        try { // To catch any exceptions that may occur during the calculation. (Like empty value in the field)
+
             firstNumberInt = Float.parseFloat(inputFirstNumber.getText().toString());
             secondNumberInt = Float.parseFloat(inputSecondNumber.getText().toString());
+            // Make input fields into floats.
+
             System.out.println("First Number: " + firstNumberInt);
             System.out.println("Second Number: " + secondNumberInt);
 
-
-            result = firstNumberInt + secondNumberInt;
+            result = firstNumberInt + secondNumberInt; // Add the two numbers together.
 
             System.out.println("Sum: " + result);
+            output.setText(String.valueOf(result)); // Display the result in the output field.
 
-            output.setText(String.valueOf(result));
         } catch (Exception e){
             Toast.makeText(this, "Please enter a valid number", Toast.LENGTH_LONG).show();
+            // If the input fields are empty, a toast message is displayed to the user.
         }
     }
 
     public void subFunction(View view){
-        try {
+        // Subtraction Function: Gets Called when the - button is pressed. Subtracts the two numbers and displays the result.
+
+        try { // To catch any exceptions that may occur during the calculation. (Like empty value in the field)
 
             firstNumberInt = Float.parseFloat(inputFirstNumber.getText().toString());
             secondNumberInt = Float.parseFloat(inputSecondNumber.getText().toString());
+            // Make input fields into floats.
+
             System.out.println("First Number: " + firstNumberInt);
             System.out.println("Second Number: " + secondNumberInt);
 
-            result = firstNumberInt - secondNumberInt;
+            result = firstNumberInt - secondNumberInt; // Subtract the two numbers.
+
             System.out.println("Subtracted Value: " + result);
-            output.setText(String.valueOf(result));
+            output.setText(String.valueOf(result)); // Display the result in the output field.
 
         } catch (Exception e){
             Toast.makeText(this, "Please enter a valid number", Toast.LENGTH_LONG).show();
+            // If the input fields are empty, a toast message is displayed to the user.
         }
     }
 
     public void multFunction(View view){
-        try {
+        // Multiplication Function: Gets Called when the X button is pressed. Multiplies the two numbers and displays the result.
+
+        try { // To catch any exceptions that may occur during the calculation. (Like empty value in the field)
 
             firstNumberInt = Float.parseFloat(inputFirstNumber.getText().toString());
             secondNumberInt = Float.parseFloat(inputSecondNumber.getText().toString());
+            // Make input fields into floats.
+
             System.out.println("First Number: " + firstNumberInt);
             System.out.println("Second Number: " + secondNumberInt);
 
-            result = firstNumberInt * secondNumberInt;
+            result = firstNumberInt * secondNumberInt; // Multiply the two numbers.
+
             System.out.println("Product: " + result);
-            output.setText(String.valueOf(result));
+            output.setText(String.valueOf(result)); // Display the result in the output field.
+
         } catch (Exception e){
             Toast.makeText(this, "Please enter a valid number", Toast.LENGTH_LONG).show();
+            // If the input fields are empty, a toast message is displayed to the user.
         }
     }
 
     public void divFunction(View view){
-        try {
+        // Division Function: Gets Called when the / button is pressed. Divides the two numbers and displays the result.
+
+        try { // To catch any exceptions that may occur during the calculation. (Like empty value in the field)
 
             firstNumberInt = Float.parseFloat(inputFirstNumber.getText().toString());
             secondNumberInt = Float.parseFloat(inputSecondNumber.getText().toString());
+            // Make input fields into floats.
+
             System.out.println("First Number: " + firstNumberInt);
             System.out.println("Second Number: " + secondNumberInt);
 
-            if (secondNumberInt == 0) {
+            if (secondNumberInt == 0) { // To check if the second number is zero.
                 Toast.makeText(this, "Cannot divide by zero", Toast.LENGTH_LONG).show();
+                // If the second number is zero, a toast message is displayed to the user.
+
             } else {
-                result = firstNumberInt / secondNumberInt;
+                result = firstNumberInt / secondNumberInt; // Divide the two numbers.
 
                 System.out.println("Division Value: " + result);
-                output.setText(String.valueOf(result));
+                output.setText(String.valueOf(result)); // Display the result in the output field.
             }
         } catch (Exception e){
             Toast.makeText(this, "Please enter a valid number", Toast.LENGTH_LONG).show();
+            // If the input fields are empty, a toast message is displayed to the user.
         }
     }
-
-
-
 }
